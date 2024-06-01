9ff2596e211fa36253044d51006dd2ad02807fc1
anishmendes <anishmainali31@gmail.com>
2024-06-01 18:57:39 +0545

    connect

--- server/Routes/record.js
+++ server/Routes/record.js
@@ -4,3 +4,18 @@ const express = require("express");
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const recordRoutes = express.Router();
+// This section will help you get a list of all the documents.
+recordRoutes.route("/listings").get(async function (req, res) {
+    const dbConnect = dbo.getDb();
+  
+    dbConnect
+      .collection("listingsAndReviews")
+      .find({}).limit(50)
+      .toArray(function (err, result) {
+        if (err) {
+          res.status(400).send("Error fetching listings!");
+       } else {
+          res.json(result);
+        }
+      });
+  });
