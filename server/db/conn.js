acc01bfbb50867cb8bd89576d2859a0b2c2ae1ad
anishmendes <anishmainali31@gmail.com>
2024-06-01 18:55:50 +0545

    connection changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,11 +1,27 @@
-import { MongoClient } from "mongodb";
-const connectionString = process.env.ATLAS_URI || "";
-const client = new MongoClient(connectionString);
-let conn;
-try {
-  conn = await client.connect();
-} catch(e) {
-  console.error(e);
-}
-let db = conn.db("sample_training");
-export default db;
+const { MongoClient } = require("mongodb");
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+let dbConnection;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return callback(err);
+      }
+ 
+      dbConnection = db.db("sample_airbnb");
+      console.log("Successfully connected to MongoDB.");
+ 
+      return callback();
+    });
+  },
+ 
+  getDb: function () {
+    return dbConnection;
+  },
+};
