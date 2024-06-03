565d98ac46e8b7d3707dec0d0015ef39e9233957
anishmendes <anishmainali31@gmail.com>
2024-06-03 10:41:23 +0545

    middleware

--- server/server.js
+++ server/server.js
@@ -4,3 +4,16 @@ const Product = require('./models/product');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Database Connection
+mongoose.connect('mongodb://localhost:27017/trekking', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Failed to connect to MongoDB', err);
+});
