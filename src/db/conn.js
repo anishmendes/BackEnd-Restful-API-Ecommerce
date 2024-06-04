e7d37fc9857ff3be4f2664def421d2025e74fb84
anishmendes <anishmainali31@gmail.com>
2024-06-04 17:40:39 +0545

    ..

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongooose');
 require("../src/db/conn");
 
-// mongoose.connect("mongodb://localhost:27017/pickle" , {
-//     useCreateIndex:true,
-//     useNewUrlParser:true,
-//     useUnifiedTopology:true
-// }).then(() => {
-//     console.log("connection sucessful");
-// }).catch((e) => {
-//     console.log("Connection Failed")
-// })
+mongoose.connect("mongodb://localhost:27017/pickle" , {
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(() => {
+    console.log("connection sucessful");
+}).catch((e) => {
+    console.log("Connection Failed")
+})
