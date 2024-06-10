23fa4a8ee0a99f38c411fd64b1d037fed5788cac
anishmendes <anishmainali31@gmail.com>
2024-06-10 15:45:21 +0545

    all done for crud and frontend connection

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require("cors");
 require("../src/db/conn")
 const Product = require("../src/models/product");
 const router = require("../src/routers/pickle")
@@ -9,6 +9,12 @@ const router = require("../src/routers/pickle")
 
 const app = express();
 const port = process.env.PORT  || 3000;
+    
+
+const allowedOrigins = ['http://localhost:5173'];
+    app.use(cors({
+      origin: allowedOrigins
+    }));
 app.use(express.json());
 app.use(router);
 
@@ -18,7 +24,6 @@ app.use(router);
 
 
 
-
 app.listen(port, () => {
     console.log(`connection is live at port no. ${port}`);
-})
+})  
